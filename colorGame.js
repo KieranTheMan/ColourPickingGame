7ee9d8795f01ca9dc5bef57cd4922d4c904805c1
KieranTheMan <kieran9fowler@gmail.com>
2020-01-06 20:16:24 +0000

    include forLoop, add colors to squares, eventListener and clickedColor===pickedColor conditional statment

--- colorGame.js
+++ colorGame.js
@@ -12,3 +12,15 @@ let pickedColor = colors[3];
 let colorDisplay = document.getElementById('colorDisplay')
 
 colorDisplay.textContent = pickedColor;
+
+for(let i = 0; i < squares.length; i++) {
+    //intial colors add to square
+    squares[i].style.background = colors[i];
+    // click listener for squares
+    squares[i].addEventListener('click', function() {
+        //get color of clicked square
+        let clickedColor = this.style.background
+        //compare color to picked color
+        clickedColor === pickedColor ? alert('correct') : alert('wrong');
+    });
+}
